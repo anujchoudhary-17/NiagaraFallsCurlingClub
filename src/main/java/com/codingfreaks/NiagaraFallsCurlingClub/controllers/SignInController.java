54edad762565ac6835ddb18148772ecea1ba1e9d
Anuj Choudhary <50038492+anujchoudhary-17@users.noreply.github.com>
2021-09-09 12:54:18 -0400

    Update SignInController.java

--- src/main/java/com/codingfreaks/NiagaraFallsCurlingClub/controllers/SignInController.java
+++ src/main/java/com/codingfreaks/NiagaraFallsCurlingClub/controllers/SignInController.java
@@ -43,7 +43,7 @@ public class SignInController {
 
 	        if (!validateUserData(model,email,password)) {
 	        
-	        	 return "redirect:/views/signIn";	 
+	        	 return "redirect:signin";	 
 	        	 }	       
 	        else
 	        {
