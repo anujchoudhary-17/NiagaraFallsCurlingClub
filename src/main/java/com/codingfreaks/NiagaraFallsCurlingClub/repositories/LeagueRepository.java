31dfb51ecf9975fd71b26ae831be82750304e818
Anuj <choudharyanuj268@gmail.com>
2021-10-12 12:32:31 -0400

    Final Sprint 4 Push

--- src/main/java/com/codingfreaks/NiagaraFallsCurlingClub/repositories/LeagueRepository.java
+++ src/main/java/com/codingfreaks/NiagaraFallsCurlingClub/repositories/LeagueRepository.java
@@ -2,7 +2,5 @@ package com.codingfreaks.NiagaraFallsCurlingClub.repositories;
 import com.codingfreaks.NiagaraFallsCurlingClub.modelClasses.League;
 import org.springframework.data.mongodb.repository.MongoRepository;
 public interface LeagueRepository extends MongoRepository<League,String>{
-  
-
     
 }
